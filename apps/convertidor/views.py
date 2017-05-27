00b9c48d61ba172624b90c82873723693182e3ff
Fernando García <fkngv_10@hotmail.com>
2017-05-27 00:36:17 -0500

    Guardar los campos y tipo en diccionario

--- apps/convertidor/views.py
+++ apps/convertidor/views.py
@@ -5,47 +5,53 @@ import csv, re, collections
 # Create your views here.
 def bd(request):
     if request.method == 'POST':
-        fields = {}
+        fields = collections.OrderedDict()
         first_field = True
         first_row = True
         first_col = True
-        db = request.POST['db'].lower().replace(" ", "_")
-        table = request.POST['table'].lower().replace(" ", "_")
-        field_name = request.POST['field_name'].lower().replace(" ", "_")
-        field_type = request.POST['field_type'].lower().replace(" ", "_")
-        fields[field_name] = field_type
-        fields = collections.OrderedDict()
+        db = request.POST['db'].lower().strip().replace(" ", "_")
+        table = request.POST['table'].lower().strip().replace(" ", "_")
+        field_name = request.POST.getlist('field_name[]')
+        field_type = request.POST.getlist('field_type[]')
 
-        file = request.FILES['file']
+        for field in field_name:
+            fn = field.lower().strip().replace(" ", "_")
+            ft = field_type[field_name.index(field)].lower().strip().replace(" ", "_")
+            fields[fn] = ft
 
-        print("\n\nCREATE DATABASE {};".format(db))
-        print("CREATE TABLE {} (".format(table))
-        for fn,ft in fields.items():
-            if first_field is True:
-                print("\t{} {}".format(fn,ft))
-                first_field = False
-            else:
-                print(",")
-                print("\t{} {}".format(fn,ft))
-        print(");")
+        for key, value in fields.items():
+            print(key, value)
 
-        with open(file, newline='') as f:
-            reader = csv.reader(f, delimiter=';')
-            for row in reader:
-                if first_row is True:
-                    first_row = False
-                    continue
+        # file = request.FILES['file']
 
-                print("INSERT INTO {} VALUES (".format(table), end="")
-                for col in row:
-                    if first_col is True:
-                        first_col = False
-                        print(col, end="")
-                    else:
-                        print(", {}".format(col), end="")
-                print(");")
-                first_col = True
-
+    #     print("\n\nCREATE DATABASE {};".format(db))
+    #     print("CREATE TABLE {} (".format(table))
+    #     for fn,ft in fields.items():
+    #         if first_field is True:
+    #             print("\t{} {}".format(fn,ft))
+    #             first_field = False
+    #         else:
+    #             print(",")
+    #             print("\t{} {}".format(fn,ft))
+    #     print(");")
+    #
+    #     with open(file, newline='') as f:
+    #         reader = csv.reader(f, delimiter=';')
+    #         for row in reader:
+    #             if first_row is True:
+    #                 first_row = False
+    #                 continue
+    #
+    #             print("INSERT INTO {} VALUES (".format(table), end="")
+    #             for col in row:
+    #                 if first_col is True:
+    #                     first_col = False
+    #                     print(col, end="")
+    #                 else:
+    #                     print(", {}".format(col), end="")
+    #             print(");")
+    #             first_col = True
+    #
         return render(request, 'forms/form_BD_response.html')
     else:
         return render(request, 'forms/form_BD.html')
