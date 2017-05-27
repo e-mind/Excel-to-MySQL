04eed22f295e3542c9ac82fc81ab83dd6e8fbeeb
Fernando García <fkngv_10@hotmail.com>
2017-05-27 03:01:39 -0500

    Creación de archivo sql

--- apps/convertidor/views.py
+++ apps/convertidor/views.py
@@ -1,64 +1,65 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 import csv, re, collections
 
 # Create your views here.
 def bd(request):
     if request.method == 'POST':
-        # fields = collections.OrderedDict()
-        # first_field = True
-        # first_row = True
-        # first_col = True
-        # db = request.POST['db'].lower().strip().replace(" ", "_")
-        # table = request.POST['table'].lower().strip().replace(" ", "_")
-        # field_name = request.POST.getlist('field_name[]')
-        # field_type = request.POST.getlist('field_type[]')
-        #
-        # for field in field_name:
-        #     fn = field.lower().strip().replace(" ", "_")
-        #     ft = field_type[field_name.index(field)].lower().strip().replace(" ", "_")
-        #     fields[fn] = ft
+        fields = collections.OrderedDict()
+        first_field = True
+        first_row = True
+        first_col = True
+        db = request.POST['db'].lower().strip().replace(" ", "_")
+        table = request.POST['table'].lower().strip().replace(" ", "_")
+        field_name = request.POST.getlist('field_name[]')
+        field_type = request.POST.getlist('field_type[]')
+
+        for field in field_name:
+            fn = field.lower().strip().replace(" ", "_")
+            ft = field_type[field_name.index(field)].lower().strip().replace(" ", "_")
+            fields[fn] = ft
 
         file = request.FILES['file']
         fs = FileSystemStorage()
         filename = fs.save(file.name, file)
-        uploaded_file_url = fs.url(filename)
 
-        print(uploaded_file_url)
+        sql = ""
+        sql += "CREATE DATABASE {};".format(db)
+        sql += "\nUSE {};".format(db)
+        sql += "\nCREATE TABLE {} (".format(table)
+        for fn,ft in fields.items():
+            if first_field is True:
+                sql += "\n\t{} {}".format(fn,ft)
+                first_field = False
+            else:
+                sql += ","
+                sql += "\n\t{} {}".format(fn,ft)
+        sql += "\n);"
 
-        # with open(filename, encoding='utf-8') as f:
-        #     for chunk in file.chunks():
-        #         print(chunk)
+        with open(settings.MEDIA_ROOT + file.name, encoding='utf-8', newline='') as f:
+            reader = csv.reader(f, delimiter=';')
+            for row in reader:
+                if first_row is True:
+                    first_row = False
+                    continue
 
-    #     print("\n\nCREATE DATABASE {};".format(db))
-    #     print("CREATE TABLE {} (".format(table))
-    #     for fn,ft in fields.items():
-    #         if first_field is True:
-    #             print("\t{} {}".format(fn,ft))
-    #             first_field = False
-    #         else:
-    #             print(",")
-    #             print("\t{} {}".format(fn,ft))
-    #     print(");")
-    #
-    #     with open(file, newline='') as f:
-    #         reader = csv.reader(f, delimiter=';')
-    #         for row in reader:
-    #             if first_row is True:
-    #                 first_row = False
-    #                 continue
-    #
-    #             print("INSERT INTO {} VALUES (".format(table), end="")
-    #             for col in row:
-    #                 if first_col is True:
-    #                     first_col = False
-    #                     print(col, end="")
-    #                 else:
-    #                     print(", {}".format(col), end="")
-    #             print(");")
-    #             first_col = True
-    #
-        return render(request, 'forms/form_BD_response.html')
+                sql += "\nINSERT INTO {} VALUES (".format(table)
+                for col in row:
+                    if first_col is True:
+                        first_col = False
+                        sql += col
+                    else:
+                        sql += ", {}".format(col)
+                sql += ");"
+                first_col = True
+
+        file_name, file_extension = file.name.split(".")
+        new_file = file_name + "_SQL.sql"
+        with open(settings.MEDIA_ROOT + new_file, 'w', encoding='utf-8', newline='') as f:
+            f.write(sql)
+
+        return render(request, 'forms/form_BD_response.html', {'sql' : sql, 'new_file' : new_file})
     else:
         return render(request, 'forms/form_BD.html')
---
+++ static/files/prueba_SQL.sql
@@ -0,0 +1,5 @@
+
+CREATE DATABASE mi_base;
+CREATE TABLE mi_tabla (
+	nombre int
+);INSERT INTO mi_tabla VALUES (1, 2, 3);INSERT INTO mi_tabla VALUES (2, 3, 4);INSERT INTO mi_tabla VALUES (3, 4, 5);INSERT INTO mi_tabla VALUES (4, 5, 6);INSERT INTO mi_tabla VALUES (5, 6, 7);INSERT INTO mi_tabla VALUES (6, 7, 8);INSERT INTO mi_tabla VALUES (7, 8, 9);INSERT INTO mi_tabla VALUES (8, 9, 10);INSERT INTO mi_tabla VALUES (9, 10, 11);INSERT INTO mi_tabla VALUES (10, 11, 12);INSERT INTO mi_tabla VALUES (11, 12, 13);INSERT INTO mi_tabla VALUES (12, 13, 14);INSERT INTO mi_tabla VALUES (13, 14, 15);
